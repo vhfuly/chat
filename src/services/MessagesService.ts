af32a0639a56c276373f89fe2106392d07a327cb
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-04-26 19:49:48 -0300

    Refactor: add contructor in services

--- src/services/MessagesService.ts
+++ src/services/MessagesService.ts
@@ -1,5 +1,6 @@
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, Repository } from 'typeorm';
 import { MessagesRepository } from '../repositories/MessagesRepository';
+import { Message } from '../entities/Message';
 
 interface IMessageCreate {
   text: string;
@@ -8,24 +9,26 @@ interface IMessageCreate {
 }
 
 class MessagesService {
-  async create ({admin_id, user_id, text}: IMessageCreate  ) {
-    const messagesRepository = getCustomRepository(MessagesRepository);
+  private messagesRepository: Repository<Message>
 
-    const message = messagesRepository.create({
+  constructor() {
+    this.messagesRepository = getCustomRepository(MessagesRepository);
+  }
+  async create ({admin_id, user_id, text}: IMessageCreate  ) {
+
+    const message = this.messagesRepository.create({
       admin_id,
       text,
       user_id
     });
   
-    await messagesRepository.save(message)
+    await this.messagesRepository.save(message)
 
     return message;
   }
 
   async listByUser( user_id: string) {
-    const messagesRepository  = getCustomRepository(MessagesRepository);
-
-    const list = await messagesRepository.find({
+    const list = await this.messagesRepository.find({
       where: { user_id },
       relations: ['user'],
     });
