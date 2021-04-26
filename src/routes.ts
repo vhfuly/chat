56146fca1a28c564f1049a0f6a6817810bb451fa
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-04-26 18:41:38 -0300

    feat: create UsersController

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
 import { SettingsController } from './Controllers/SettingsController';
+import { UsersController } from './Controllers/UsersController';
 
 const routes = Router();
 
 const settingsController = new SettingsController();
+const usersController = new UsersController();
+
 
 routes.post('/settings', settingsController.create)
+routes.post('/users', usersController.create)
 
 export { routes }
