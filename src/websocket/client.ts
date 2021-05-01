4975f1fa1b9d1f5270175213b2e8f596a7f0a500
Victor Fuly <victor_hugo_fuly@hotmail.com>
2021-05-01 09:42:37 -0300

    Feat: save connections

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,7 +1,36 @@
 import { io } from '../http';
+import { ConnectionsService } from '../services/ConnectionsService';
+import { UsersService } from '../services/UsersService';
 
 io.on('connect', (socket) => {
-  socket.on('client_first_access', params => {
-    console.log(params);
+  const connectionsService = new ConnectionsService();
+  const usersService = new UsersService();
+
+  socket.on('client_first_access', async (params) => {
+    const socket_id = socket.id
+    const { text, email} = params;
+
+    const userExists = await usersService.findByEmail(email);
+
+    if (!userExists) {
+      const user = await usersService.create(email);
+      await connectionsService.create({
+        socket_id,
+        user_id: user.id,
+      })
+    } else {
+      const connection = await connectionsService.findByUserId(userExists.id);
+
+      if (!connection){
+        await connectionsService.create({
+          socket_id,
+          user_id: userExists.id,
+        })
+      } else {
+        connection.socket_id = socket_id;
+        await connectionsService.create(connection)
+      }
+    }
+
   })
 })
